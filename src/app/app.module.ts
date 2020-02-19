8853c2e384b4017cebe11b94620464d0a030616c
Naman Rastogi <naman815@gmail.com>
2020-02-20 01:35:47 +0530

    angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,27 @@ import { AppComponent } from "./app.component";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
 
 import { DishService } from "./services/dish.service";
 
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -25,7 +41,8 @@ import { DishService } from "./services/dish.service";
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
