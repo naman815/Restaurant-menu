5531fac20e58ee0213d28ceaa2a6d1e5085bbc9a
Naman Rastogi <naman815@gmail.com>
2020-03-03 02:02:10 +0530

    delay and spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatSelectModule } from "@angular/material/select";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 import { AppComponent } from "./app.component";
 
@@ -62,7 +63,8 @@ import { LoginComponent } from "./login/login.component";
     FormsModule,
     ReactiveFormsModule,
     MatSelectModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,7 +20,7 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     const id = this.route.snapshot.params["id"];
-    console.log(id);
+
     this.dishservice.getDish(id).then(dishes => (this.dish = dishes));
   }
   goBack(): void {
